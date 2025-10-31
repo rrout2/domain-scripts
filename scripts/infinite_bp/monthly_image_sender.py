a8fdadd480da53bd3f759f81361d88624d81d27b
Rishav Rout <rout.rishav@gmail.com>
2025-10-31 13:45:38 -0700

    week 9

--- scripts/infinite_bp/monthly_image_sender.py
+++ scripts/infinite_bp/monthly_image_sender.py
@@ -176,7 +176,7 @@ class ImageEmailSender:
         try:
             # Navigate to the website
             if weekly_algo:
-                url = self.construct_url(idx, manual, week=8)
+                url = self.construct_url(idx, manual, week=9)
             else:
                 url = self.construct_url(idx, manual, week=0)
             print(f"Navigating to {url}")
